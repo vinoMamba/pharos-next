59d4738cbadee910d098ec5e3d623689c71b4eb5
wangxin <vino0908@outlook.com>
2023-06-26 14:50:35 +0800

    chore: 修改eslint 规则

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,9 +6,12 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-typescript'
+    '@vue/eslint-config-typescript',
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'no-case-declarations': 'off',
   }
 }
